28531d58d5ec0fa52bc25c340391f99ae4745803
Benjamin Staneck <staneck@gmail.com>
2016-06-23 20:58:52 +0200

    Legion

--- MediaCore.lua
+++ MediaCore.lua
@@ -1,7 +1,4 @@
 ﻿local LSM3 = LibStub("LibSharedMedia-3.0", true)
-local LSM2 = LibStub("LibSharedMedia-2.0", true)
-local SML = LibStub("SharedMedia-1.0", true)
-local Surface = LibStub("Surface-1.0", true)
 
 SharedMedia = {}
 SharedMedia.revision = tonumber(string.sub("$Revision: 63552 $", 12, -3)) or 1
@@ -11,16 +8,7 @@ SharedMedia.registry = { ["statusbar"] = {} }
 function SharedMedia:Register(mediatype, key, data, langmask)
 	if LSM3 then
 		LSM3:Register(mediatype, key, data, langmask)
-	end
-	if LSM2 then
-		LSM2:Register(mediatype, key, data)
-	end
-	if SML then
-		SML:Register(mediatype, key, data)
-	end
-	if Surface and mediatype == "statusbar" then
-		Surface:Register(key, data)
-	end
+	end	
 	if not SharedMedia.registry[mediatype] then
 		SharedMedia.registry[mediatype] = {}
 	end
@@ -37,35 +25,7 @@ function SharedMedia.OnEvent(this, event, ...)
 				end
 			end
 		end
-	end
-	if not LSM2 then
-		LSM2 = LibStub("LibSharedMedia-2.0", true)
-		if LSM2 then
-			for m,t in pairs(SharedMedia.registry) do
-				for _,v in ipairs(t) do
-					LSM2:Register(m, v[1], v[2])
-				end
-			end
-		end
-	end
-	if not SML then
-		SML = LibStub("SharedMedia-1.0", true)
-		if SML then
-			for m,t in pairs(SharedMedia.registry) do
-				for _,v in ipairs(t) do
-					SML:Register(m, v[1], v[2])
-				end
-			end
-		end
-	end
-	if not Surface then
-		Surface = LibStub("Surface-1.0", true)
-		if Surface then
-			for k,v in ipairs(SharedMedia.registry.statusbar) do
-				Surface:Register(v[1], v[2])
-			end
-		end
-	end
+	end	
 end
 
 SharedMedia.frame = CreateFrame("Frame")
